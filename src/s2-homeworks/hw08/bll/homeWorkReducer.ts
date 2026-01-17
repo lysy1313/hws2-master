5c35d15f0f1a0f83790d89eef3cc2486c14354bb
lysy <lysy.ilya1313@gmail.com>
2026-01-18 00:25:26 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,34 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+        return state.sort((people1, people2) =>
+          people1.name.localeCompare(people2.name, "ru"),
+        );
+      }
+      if (action.payload === "down") {
+        return state.sort((people1, people2) =>
+          people2.name.localeCompare(people1.name, "ru"),
+        );
+      }
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      return state; // need to fix
     }
-}
+    case "check": {
+      return state.filter((el) => el.age > 18); // need to fix
+    }
+    default:
+      return state;
+  }
+};
